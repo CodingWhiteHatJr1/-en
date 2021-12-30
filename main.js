1a9915cbf4c78211d42c6a0bc38c951f80c25f0f
CodingWhiteHatJr1 <83186646+CodingWhiteHatJr1@users.noreply.github.com>
2021-12-29 20:19:46 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,99 +1,65 @@
-var mouseEvent = "empty";
-var last_position_of_x, last_position_of_y;
 
-    canvas = document.getElementById('myCanvas');
-    ctx = canvas.getContext("2d");
-    
-    color = "black";
-    width_of_line = 1;
+var canavs= new fabric.Canvas("myCanvas");
+ block_y=1;
+ block_x=1;
 
-    canvas.addEventListener("mousedown", my_mousedown);
-    
-    function my_mousedown(e)
-    {
-        //Addictonal Activity start
-        color = document.getElementById("color").value;
-        width_of_line = document.getElementById("width_of_line").value;
-        //Addictonal Activity ends
+block_image_width = 350;
+block_image_height = 430;
 
-        mouseEvent = "mouseDown";
-    }
+var block_image_object= "";
 
-    canvas.addEventListener("mousemove", my_mousemove);
-    function my_mousemove(e)
-    {
-        current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
-        current_position_of_mouse_y = e.clientY - canvas.offsetTop;
-
-        if (mouseEvent == "mouseDown") {
-        ctx.beginPath();
-        ctx.strokeStyle = color;
-        ctx.lineWidth = width_of_line;
-        ctx.moveTo(last_position_of_x, last_position_of_y);
-        ctx.lineTo(current_position_of_mouse_x, current_position_of_mouse_y);
-        ctx.stroke();
-        }
-
-        last_position_of_x = current_position_of_mouse_x; 
-        last_position_of_y = current_position_of_mouse_y;
-    }
-
-    canvas.addEventListener("mouseup", my_mouseup);
-    function my_mouseup(e)
-    {
-        mouseEvent = "mouseUP";
-    }
-
-    canvas.addEventListener("mouseleave", my_mouseleave);
-    function my_mouseleave(e)
-    {
-        mouseEvent = "mouseleave";
-    }
-
-var last_position_of_touch_x, last_position_of_touch_y;
-var width = screen.width;
-
-new_width =  screen.width - 70; 
-new_height = screen.height - 300;
-    if(width < 992)
-    {
-    document.getElementById("myCanvas").width = new_width;
-    document.getElementById("myCanvas").height = new_height;
-    document.body.style.overflow = "hidden";
-    }
-    canvas.addEventListener("touchstart", my_touchstart);
-
-function my_touchstart(e) 
+function new_image(get_image)
 {
-    console.log("my_touchstart");
-  //Additional Activity
-  color = document.getElementById("color").value;
-  width_of_line = document.getElementById("width_of_line").value;
-  //End Additional Activity
-         
-    last_position_of_touch_x = e.touches[0].clientX - canvas.offsetLeft;
-    last_position_of_touch_y = e.touches[0].clientY - canvas.offsetTop;
+	fabric.Image.fromURL(get_image, function(Img){
+		block_image_object= Img;
+
+		block_image_object.scaleToWidth(block_image_width);
+		block_image_object.scaleToHeight(block_image_height);
+		block_image_object.set({
+			top:block_y,
+			left:block_x
+			});
+			canvas.add(block_image_object);
+	});
 }
 
-canvas.addEventListener("touchmove", my_touchmove);
+window.addEventListener("keydown", my_keydown);
 
-function my_touchmove(e) 
+function my_keydown(e)
 {
-    current_position_of_touch_x = e.touches[0].clientX - canvas.offsetLeft;
-    current_position_of_touch_y = e.touches[0].clientY - canvas.offsetTop;
-    ctx.beginPath();
-    ctx.strokeStyle = color;
-    ctx.lineWidth = width_of_line;
-    ctx.moveTo(last_position_of_touch_x, last_position_of_touch_y);
-    ctx.lineTo(current_position_of_touch_x, current_position_of_touch_y);
-    ctx.stroke();
+keyPressed = e.keyCode;
+console.log(keyPressed);
 
-    last_position_of_touch_x = current_position_of_touch_x; 
-    last_position_of_touch_y = current_position_of_touch_y;
-   
+	if(keyPressed == '82') 
+	{
+		new_image('rr1.png');
+		console.log("r");
+	}
+	if(keyPressed == '71')
+	{
+		block_x = 200;
+		new_image('gr.png');
+		console.log("g");
+	}
+	
+	if(keyPressed == '89')
+	{
+		block_x =350;
+		new_image('yr.png');
+		console.log("y");
+	}
+	if(keyPressed == '80')
+	{
+		block_x = 600;
+		new_image('pr.png');
+		console.log("p");
+	}
+	if(keyPressed == '66')
+	{
+		block_x = 700;
+		new_image('br.png');
+		console.log("b");
+	}
+	
 }
 
-//Additional Activity
-function clearArea() {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-}
